b7b460517f31529a9af02173bd7769e1f6346be9
emirortahisar <79744450+emirortahisar@users.noreply.github.com>
2022-05-14 21:58:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title('My Parent New Healty Dinner')
 
-
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')   🥣 🥗 🐔 🥑🍞
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
