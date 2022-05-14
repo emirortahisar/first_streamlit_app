4263959555579efc867091fd14dc95356f45c99a
emirortahisar <79744450+emirortahisar@users.noreply.github.com>
2022-05-14 21:34:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,4 @@ import streamlit
 
 streamlit.title('My Parent New Healty Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+
