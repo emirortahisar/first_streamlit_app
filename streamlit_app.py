3818789b33c8ae5d68e7483da72253deb0042ad1
emirortahisar <79744450+emirortahisar@users.noreply.github.com>
2022-05-20 22:58:43 +0300

    Update streamlit_app.py
    kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ streamlit.dataframe(fruits_to_show)
 
 
 import requests
-fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #writes data to screen
+fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/"+"kiwi")
+
 
 #take the json version and normalize it 
 fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())
