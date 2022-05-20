eac7394ffd5f44ac165ec35bb9159bb627b971df
emirortahisar <79744450+emirortahisar@users.noreply.github.com>
2022-05-20 22:56:13 +0300

    Update streamlit_app.py
    screen as a  table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,8 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json()) #writes data to screen
+
+#take the json version and normalize it 
+fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())
+#output is the screen as a  table 
