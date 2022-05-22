dce9c2b90aaf17eacc0f03a9c8d220bca7a6eda9
emirortahisar <79744450+emirortahisar@users.noreply.github.com>
2022-05-22 20:10:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,8 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The Fruit Load List contains:")
 streamlit.dataframe(my_data_row)
 
-import pandas
-add_my_list=my_data_row
+
+add_my_list=my_curr
 add_my_list=add_my_list.set_index('Fruitn')
 fruits_selected_n=streamlit.multiselect("Pick some fruits:",list(add_my_list.index),['Avocado','Strawberries'])
 fruits_to_show_n = add_my_list.loc[fruits_selected_n]
