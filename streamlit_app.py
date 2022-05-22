a4fb346f26f5f3e1d2ed19b2c5c1dc0d97a34636
emirortahisar <79744450+emirortahisar@users.noreply.github.com>
2022-05-22 21:58:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,21 +28,59 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-#new section to display frity api resource 
+
+
+
+
+
+
+
+
+
+
+
+
+# creeate a fucntion
+def get_fruityvice_data(this_friut_choice):
+   fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/"+fruit_choice)
+      fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+  
+  
+
 streamlit.header('Fruitvice Fuit Advice!')
 try:
   fruit_choice=streamlit.text_input('what fruit you like information about ?','Kiwi')
   if not fruit_choice:
     streamlit.error("pelease enter a fruit to get information")
   else:
-      fruityvice_response=requests.get("https://www.fruityvice.com/api/fruit/"+fruit_choice)
-      fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+      
 except URLError as e:
   streamlit.error()
 
+  
 #dont run 
 streamlit.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #import snowflake.connector
 
